3b1a7958b9dcc56394430852cd449c759246d01b
u0_a125 <u0_a125@localhost>
2022-02-25 20:37:41 +0300

    new update

--- kurulum.sh
+++ kurulum.sh
@@ -4,8 +4,35 @@ if [[ -a $HOME/.termux ]];then
 	rm -rf $HOME/.termux
 fi
 mv termux $HOME/.termux
-echo "#!/bin/bash\ncd $HOME/.termux\nbash termux-styling.sh $1" > $PREFIX/bin/termux-styling
+echo -e "#!/bin/bash\ncd $HOME/.termux\nbash termux-styling.sh $1" > $PREFIX/bin/termux-styling
 chmod 777 $PREFIX/bin/termux-styling
 cd ..
 rm -rf TERMUX-STYLING
+
+echo
+echo
+echo
+echo
+printf "\e[32m
+\t\t╔════════════════════════╗
+\t\t║                        ║
+\t\t║   \e[97mＫＵＲＵＬＵＭ\e[32m       ║
+\t\t║                        ║
+\t\t║   \e[97mＴＡＭＡＭＬＡＮＤＩ\e[32m ║
+\t\t║                        ║
+\t\t╚════════════════════════╝
+\n\n\e[97m"
+sleep 1
+
+echo
+echo
+echo
+printf "\e[32m
+\t\t╔═════════════════════════════════════╗
+\t\t║ \e[97mÇALIŞTIRMAK İÇİN \e[32mtermux-styling \e[97mYAZ \e[32m║
+\t\t╚═════════════════════════════════════╝\e[97m"
+echo
+echo
+echo
+exit
 
