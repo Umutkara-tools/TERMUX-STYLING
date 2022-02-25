8a8b1a75b08c4f7c9693c8542632f26f039c32c3
u0_a125 <u0_a125@localhost>
2022-02-25 20:29:10 +0300

    new update

--- kurulum.sh
+++ kurulum.sh
@@ -5,6 +5,7 @@ if [[ -a $HOME/.termux ]];then
 fi
 mv termux $HOME/.termux
 echo "#!/bin/bash\ncd $HOME/.termux\nbash termux-styling.sh $1" > $PREFIX/bin/termux-styling
+chmod 777 $PREFIX/bin/termux-styling
 cd ..
 rm -rf TERMUX-STYLING
 
